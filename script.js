54cfcb2953f260acc0bd80dce57434fcfe905227
Margo <example@gmail.com>
2020-04-20 10:46:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var leftBorderWidht = 1;
+{
+  let second = 2;
+  console.log(second)
+}
+const t = 3.14;
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+console.log(persone.name);
+
+let answer = confirm("Are you here?");
+console.log(answer);
+
+let incr = 10,
+    decr = 10;
+  incr++;
+  decr--;
+console.log(incr);
+console.log(decr);
